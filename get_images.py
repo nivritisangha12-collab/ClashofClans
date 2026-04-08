f4aa2be9124dcff6f1ee000b99b3dab178ec9ba9
nivritisangha12-collab <nivritisangha126@gmail.com>
2026-04-08 23:45:24 +0530

    Create get_images.py
    images of all the troops

---
+++ get_images.py
@@ -0,0 +1,13 @@
+import urllib.request
+import json
+
+troops = [
+    "Barbarian_King", "Archer_Queen", "Grand_Warden", "Royal_Champion",
+    "Barbarian", "Archer", "Giant", "Goblin", "Wall_Breaker", "Balloon",
+    "Wizard", "Healer", "Dragon", "P.E.K.K.A", "Baby_Dragon", "Miner",
+    "Electro_Dragon", "Yeti", "Dragon_Rider", "Electro_Titan", "Root_Rider",
+    "Minion", "Hog_Rider", "Valkyrie", "Golem", "Witch", "Lava_Hound",
+    "Bowler", "Ice_Golem", "Headhunter", "Apprentice_Warden"
+]
+
+print(troops)
